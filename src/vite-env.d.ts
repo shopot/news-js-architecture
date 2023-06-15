547354ea7e0209d53a3573650517913108d48e3f
Sapronov Konstantin <ks.costas@gmail.com>
2023-06-15 14:42:59 +0300

    feat: add types for ImportMetaEnv

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_KEY: string;
+}
+
+// eslint-disable-next-line no-unused-vars
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
