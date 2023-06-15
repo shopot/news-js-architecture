179e4bc92580f6279da661d47fdfafb294906e97
Sapronov Konstantin <ks.costas@gmail.com>
2023-06-15 15:07:00 +0300

    fix: add proxy for deploy

--- src/shared/config/base.ts
+++ src/shared/config/base.ts
@@ -1,5 +1,5 @@
 export const API_URL = import.meta.env.PROD
-  ? 'https://newsapi.org/v2'
-  : 'https://rss-news-api.onrender.com/';
+  ? import.meta.env.VITE_API_PROXY_URL
+  : 'https://newsapi.org/v2';
 
 export const API_KEY_OPTIONS = `apiKey=${import.meta.env.VITE_API_KEY}`;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_API_KEY: string;
+  readonly VITE_API_PROXY_URL: string;
 }
 
 // eslint-disable-next-line no-unused-vars
